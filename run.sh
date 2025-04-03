bce15821a6e19521490b3eeb1a287c25e38af1bc
Extner32 <extner.zip@gmail.com>
2025-04-03 15:49:32 +0000

    server icon test

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #kill all previous screens
 pkill screen
 
+#pull the (new) icon and server properties from github
+REPO_OWNER="Extner32"
+REPO_NAME="mcserver"
+BRANCH="main"
+
+RAW_URL="https://raw.githubusercontent.com/$REPO_OWNER/$REPO_NAME/$BRANCH"
+
+# Download the file, overwriting the existing one
+curl -o server-icon.png "$RAW_URL/server-icon.png"
+curl -o server.properties "$RAW_URL/server.properties"
+
+
 #start playit in seperate screen and detach immediately:
 screen -dmS playit playit
 #wait for playit to start the tunnel
